fe5f5627ee926317cd68e932b799d1d00c17f784
felix <felix@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-14 05:53:26 +0000

    new function na.aggregate
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@644 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/na.aggregate.R
@@ -0,0 +1,16 @@
+na.aggregate <- function(object, ...) UseMethod("na.aggregate")
+
+## fills NA values with some aggregated function of the data.
+## generalises imputing by the overall mean, by calendar month, etc.
+na.aggregate.default <- function(object, by = 1, ..., FUN = mean, na.rm = FALSE) {
+    if (is.function(by)) by <- by(time(object), ...)
+    ## applied to each aggregated group in each series:
+    f <- function(x)
+        replace(x, is.na(x), FUN(x[!is.na(x)]))
+    na.aggregate.0 <- function(y) {
+        ave(y, by, FUN = f)
+    }
+    object[] <- if (length(dim(object)) == 0) na.aggregate.0(object)
+                else apply(object, 2, na.aggregate.0)
+    if (na.rm) na.omit(object) else object
+}
