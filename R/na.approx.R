066929c5fd56f8184063d16323fe3ff5d8975a55
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-04 03:14:33 +0000

    bug fixes to na.approx.* and na.spline.*.  New na.approx.{zoo, zooreg} and na.spline.{zoo, zooreg}.
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@655 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/na.approx.R
+++ R/na.approx.R
@@ -1,28 +1,42 @@
 na.approx <- function(object, ...) UseMethod("na.approx")
 
+na.approx.zoo <- function(object, along = index(object), na.rm = TRUE, ...) {
+	result <- na.approx.default(object, along = along, na.rm = FALSE, ...)
+	result <- zoo(result, along)
+    if (na.rm) {
+            out <- na.omit(result)
+            attr(out, "na.action") <- NULL
+            out
+    } else result
+}
+
+na.approx.zooreg <- function(object, along = index(object), na.rm = TRUE, ...) {
+	object0 <- structure(object, class = setdiff(class(object), "zooreg"))
+	as.zooreg(na.approx(object0, along = along, na.rm = na.rm, ...), 
+		frequency = frequency(object))
+}
+
 # interpolates object along along which defaults to index(object)
 # along has to be numeric, is otherwise coerced
-na.approx.default <- function(object, along = index(object), na.rm = TRUE, 
-                              maxGapFilled = Inf, ...)
+na.approx.default <- function(object, along = index(object), na.rm = TRUE, maxGapFilled = Inf, ...)
 {
-	object.0 <- if (missing(along)) object else window(object, index = along)
-	along <- as.numeric(along)
-	along.index <- as.numeric(time(object))
+	along.numeric <- as.numeric(along)
+	object.index <- as.numeric(time(object))
 	na.approx.0 <- function(y) {
 		na <- is.na(y)
 		if(all(!na)) return(y)
-		# y[na] <- approx(along.index[!na], y[!na], along[na], ...)$y
-		yf <- approx(along.index[!na], y[!na], along, ...)$y
-                fillShortGaps(y, yf, maxGapFilled = maxGapFilled)
+		# y[na] <- approx(along.index[!na], y[!na], along.numeric[na], ...)$y
+		yf <- approx(object.index[!na], y[!na], along.numeric, ...)$y
+		fillShortGaps(y, yf, maxGapFilled = maxGapFilled)
 	}
 
-        object.0[] <- if (length(dim(object)) == 0) na.approx.0(object)
-        	else apply(object, 2, na.approx.0)
+        result <- structure(if (length(dim(object)) == 0) na.approx.0(object)
+        	else apply(object, 2, na.approx.0), class = class(object))
         if (na.rm) {
-            out <- na.omit(object.0)
+            out <- na.omit(result)
             attr(out, "na.action") <- NULL
             out
-        } else object.0
+        } else result
 }
 
 fillShortGaps <- function(x, fill, maxGapFilled = 1)
@@ -36,3 +50,4 @@ fillShortGaps <- function(x, fill, maxGapFilled = 1)
     naok <- inverse.rle(naruns)
     ifelse(naok, fill, x)
 }
+
--- R/na.spline.R
+++ R/na.spline.R
@@ -1,22 +1,43 @@
 na.spline <- function(object, ...) UseMethod("na.spline")
 
+na.spline <- function(object, ...) UseMethod("na.spline")
+
+na.spline.zoo <- function(object, along = index(object), na.rm = TRUE, ...) {
+	result <- na.spline.default(object, along = along, na.rm = FALSE, ...)
+	result <- zoo(result, along)
+    if (na.rm) {
+            out <- na.omit(result)
+            attr(out, "na.action") <- NULL
+            out
+    } else result
+}
+
+na.spline.zooreg <- function(object, along = index(object), na.rm = TRUE, ...) {
+	object0 <- structure(object, class = setdiff(class(object), "zooreg"))
+	as.zooreg(na.spline(object0, along = along, na.rm = na.rm, ...), 
+		frequency = frequency(object))
+}
+
 # interpolates object along along which defaults to index(object)
 # along has to be numeric, is otherwise coerced
-na.spline.default <- function(object, along = index(object), na.rm = TRUE, 
-                              maxGapFilled = Inf, ...)
+na.spline.default <- function(object, along = index(object), na.rm = TRUE, maxGapFilled = Inf, ...)
 {
-	object.0 <- if (missing(along)) object else window(object, index = along)
-	along <- as.numeric(along)
-	along.index <- as.numeric(time(object))
+	along.numeric <- as.numeric(along)
+	object.index <- as.numeric(time(object))
 	na.spline.0 <- function(y) {
 		na <- is.na(y)
 		if(all(!na)) return(y)
-		yf <- splinefun(along.index[!na], y[!na], ...)(along)
-                fillShortGaps(y, yf, maxGapFilled = maxGapFilled)
+		yf <- splinefun(object.index[!na], y[!na], ...)(along)
+		fillShortGaps(y, yf, maxGapFilled = maxGapFilled)
 	}
 
-        object.0[] <- if (length(dim(object)) == 0) na.spline.0(object)
-        	else apply(object, 2, na.spline.0)
-        if (na.rm) na.omit(object.0) else object.0
+        result <- structure(if (length(dim(object)) == 0) na.spline.0(object)
+        	else apply(object, 2, na.spline.0), class = class(object))
+        if (na.rm) {
+            out <- na.omit(result)
+            attr(out, "na.action") <- NULL
+            out
+        } else result
 }
+
 
