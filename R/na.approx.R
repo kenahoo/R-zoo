d7f9a645605a49bcf406b8514170fa2c8cb6df5d
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-23 13:56:49 +0000

    updated na.approx.Rd
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@672 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/na.approx.R
+++ R/na.approx.R
@@ -1,18 +1,25 @@
+
 na.approx <- function(object, ...) UseMethod("na.approx")
 
-na.approx.zoo <- function(object, along = index(object), na.rm = TRUE, ...) {
+na.approx.zoo <- function(object, x = index(object), na.rm = TRUE, xout = x, order.by = if (missing(xout) || is.null(xout)) x else if (identical(class(xout), class(index(object)))) xout else index(object), along, ...) {
 
-	if (missing(along)) {
-		result <- object
-		result[] <- na.approx.default(object, na.rm = FALSE, ...)
-	} else {
-
-		objectm <- merge(object, zoo(, along))
-		if (length(dim(objectm)) == 2) colnames(objectm) <- colnames(object)
-
-		result <- window(objectm, index = along)
-		result[] <- na.approx.default(objectm, along = along, na.rm = FALSE, ...)
+	if (!missing(along)) {
+		warning("along to be deprecated - use x instead")
+		if (missing(x)) x <- along
 	}
+
+	if (missing(xout) || is.null(xout) || identical(xout, index(object))) {
+		result <- object
+	} else {
+		object.x <- object
+		index(object.x) <- x
+		objectm <- merge(object.x, zoo(, xout))
+		if (length(dim(objectm)) == 2) colnames(objectm) <- colnames(object)
+		result <- window(objectm, index = xout)
+	}
+	result[] <- na.approx.default(object, x = x, na.rm = FALSE, 
+		xout = xout, ...)
+	if (!missing(order.by) && !is.null(order.by)) index(result) <- order.by
 
     if (na.rm) {
             result <- na.omit(result)
@@ -23,38 +30,48 @@ na.approx.zoo <- function(object, along = index(object), na.rm = TRUE, ...) {
 
 }
 
-na.approx.zooreg <- function(object, along = index(object), na.rm = TRUE, ...) {
+na.approx.zooreg <- function(object, ...) {
 	object. <- structure(object, class = setdiff(class(object), "zooreg"))
-	as.zooreg(na.approx(object., along = along, na.rm = na.rm, ...))
+	as.zooreg(na.approx(object., ...)
 }
 
-na.approx.default <- function(object, along = index(object), na.rm = TRUE, maxgap = Inf, x = time(object), ...) {
 
-	na.approx.vec <- function(x, y, along, ...) {
+na.approx.default <- function(object, x = index(object), na.rm = TRUE, xout = x, maxgap = Inf, along, ...) {
+
+	if (!missing(along)) {
+		warning("along to be deprecated - use x instead")
+		if (missing(x)) x <- along
+	}
+
+	na.approx.vec <- function(x, y, xout = x, ...) {
 		na <- is.na(y)
-		yf <- approx(x[!na], y[!na], along, ...)$y
+		yf <- approx(x[!na], y[!na], xout, ...)$y
 		if (maxgap < length(y)) {
 		    ## construct version of y with only gaps > maxgap
-		    ygap <- .fill_short_gaps(y, seq_along(y), maxgap = maxgap)
-		    ## construct y values at 'along', keeping NAs from ygap
+		    ygap <- .fill_short_gaps(y, seq_x(y), maxgap = maxgap)
+		    ## construct y values at 'x', keeping NAs from ygap
 		    ## (approx() does not allow NAs to be propagated)
-		    ialong <- approx(x, seq_along(y), along, ...)$y
-		    yalong <- ifelse(is.na(ygap[floor(ialong)] + ygap[ceiling(ialong)]),
+		    ix <- approx(x, seq_x(y), xout, ...)$y
+		    yx <- ifelse(is.na(ygap[floor(ix)] + ygap[ceiling(ix)]),
 					NA, yf)
-		    yalong
+		    yx
 		} else {
 		    yf
 		}
 	}
 
+	if (!identical(length(x), length(index(object)))) {
+		stop("x and index must have the same length")
+	}
 	x. <- as.numeric(x)
-	along. <- as.numeric(along)
+	if (missing(xout) || is.null(xout)) xout <- x.
+	xout. <- as.numeric(xout)
 	object. <- coredata(object)
 
 	result <- if (length(dim(object.)) < 2) {
-		na.approx.vec(x., coredata(object.), along = along., ...)
+		na.approx.vec(x., coredata(object.), xout = xout., ...)
 	} else {
-		apply(coredata(object.), 2, na.approx.vec, x = x., along = along., ...)
+		apply(coredata(object.), 2, na.approx.vec, x = x., xout = xout., ...)
 	}
 
     if (na.rm) {
@@ -72,8 +89,7 @@ na.approx.ts <- function(object, ...) {
 
 ## x = series with gaps
 ## fill = same series with filled gaps
-.fill_short_gaps <- function(x, fill, maxgap)
-{
+.fill_short_gaps <- function(x, fill, maxgap) {
     if (maxgap <= 0)
         return(x)
     if (maxgap >= length(x))
@@ -83,4 +99,6 @@ na.approx.ts <- function(object, ...) {
     naok <- inverse.rle(naruns)
     ifelse(naok, fill, x)
 }
+
+
 
