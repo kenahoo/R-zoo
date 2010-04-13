f38e67df1cd77b5113020a5c2f78b34220aefb5c
felix <felix@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-13 00:42:29 +0000

    added xblocks
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@694 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/xblocks.R
@@ -0,0 +1,78 @@
+
+xblocks <- function(x, ...)
+    UseMethod("xblocks")
+
+xblocks.default <-
+    function (x, y, ..., gaps = FALSE,
+              ybottom = par("usr")[3], ytop = ybottom + height,
+              height = diff(par("usr")[3:4]),
+              col = NULL, border = NA, 
+              last.step = median(diff(tail(x))))
+{
+    x <- as.numeric(x)
+    if (length(x) == 0) return()
+    if (is.unsorted(x, na.rm = TRUE))
+        stop("'x' should be ordered (increasing)")
+    if (is.na(last.step))
+        last.step <- 0
+    ## this will convert factor to character:
+    y <- as.vector(y)
+    ## gaps: can't just call is.na() on the input because
+    ## zoo and ts objects lose their time attributes.
+    if (gaps)
+        y <- is.na(y)
+    ## Three cases:
+    ## (1) If y is character, assume it gives the block colours
+    ## -- unless 'col' is given, which over-rides it.
+    ## (2) If y is logical, show blocks of TRUE values.
+    ## (3) If y is numeric, show blocks of non-NA values.
+    if (mode(y) == "numeric") ## includes Date etc
+        y <- !is.na(y)
+    ## Note: rle treats each NA as unique (does not combine runs of NAs)
+    ## so we need to replace NAs with a temporary value.
+    NAval <-
+        if (is.character(y)) "" else FALSE
+    y[is.na(y)] <- NAval
+    ## find blocks (runs of constant values)
+    yrle <- rle(y)
+    ## substitute NA values back in
+    blockCol <- yrle$values
+    blockCol[blockCol == NAval] <- NA
+    ## for logical series, col default comes from palette()
+    if (is.logical(y) && is.null(col))
+        col <- palette()[1]
+    ## set block colours if 'col' given
+    if (length(col) > 0) {
+        ok <- !is.na(blockCol)
+        blockCol[ok] <- rep(col, length = sum(ok)) ## rep to avoid warnings
+    }
+    ## work out block geometry
+    idxBounds <- cumsum(c(1, yrle$lengths))
+    idxStart <- head(idxBounds, -1)
+    idxEnd <- tail(idxBounds, -1)
+    idxEnd[length(idxEnd)] <- length(y)
+    blockStart <- x[idxStart]
+    blockEnd <- x[idxEnd]
+    blockEnd[length(blockEnd)] <- tail(blockEnd, 1) + last.step
+    blockWidth <- blockEnd - blockStart
+    ## adjust for log scales
+    if (par("ylog")) {
+        ybottom <- 10^ybottom
+        ytop <- 10^ytop
+    }
+    ## draw it
+    rect(xleft = blockStart, xright = blockEnd,
+         ybottom = ybottom, ytop = ytop,
+         col = blockCol, border = border, ...)
+}
+
+xblocks.zoo <-
+xblocks.ts <-
+    function(x, y = NULL, ...)
+{
+    if (!is.null(y)) {
+        xblocks.default(x, y, ...)
+    } else {
+        xblocks.default(as.vector(time(x)), as.vector(x), ...)
+    }
+}
