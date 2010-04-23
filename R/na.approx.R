5f638e4423f0191ce6841bf7cfbcbc29f6011a8d
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-23 07:43:27 +0000

    o small touch-ups in na.approx comments/docs o removed COPYRIGHTS and COPYING files because both are implicit   from DESCRIPTION o touch-ups in NEWS and DESCRIPTION for CRAN release of 1.6-3
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@706 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/na.approx.R
+++ R/na.approx.R
@@ -1,103 +1,101 @@
-
 na.approx <- function(object, ...) UseMethod("na.approx")
 
-# na.approx.zoo <- function(object, x = index(object), na.rm = TRUE, xout = x, order.by = if (missing(xout) || is.null(xout)) x else if (identical(class(xout), class(index(object)))) xout else index(object), along, ...) {
 na.approx.zoo <- function(object, x = index(object), xout, ..., na.rm = TRUE, along) {
 
-	if (!missing(along)) {
-		warning("along to be deprecated - use x instead")
-		if (missing(x)) x <- along
-	}
-
-	missing.xout <- missing(xout) || is.null(xout)
-	if (is.function(x)) x <- x(index(object))
-	if (!missing.xout && is.function(xout)) xout <- xout(index(object))
-	order.by <- if (missing.xout) index(object) else xout
-	xout <- if (missing.xout) x else xout
-
-	if (missing.xout || identical(xout, index(object))) {
-		result <- object
-	} else {
-		object.x <- object
-		if (!identical(class(x), class(xout))) {
-			index(object.x) <- as.numeric(x)
-			xout <- as.numeric(xout)
-		} else {
-			index(object.x) <- x
-		}
-		objectm <- merge(object.x, zoo(, xout))
-		if (length(dim(objectm)) == 2) colnames(objectm) <- colnames(object)
-		result <- window(objectm, index = xout)
-	}
-	result[] <- na.approx.default(object, x = x, xout = xout, na.rm = FALSE, ...)
-	if ((!missing(order.by) && !is.null(order.by)) || !missing.xout) {
-		index(result) <- order.by
-	}
-
-    if (na.rm) {
-            result <- na.omit(result)
-            attr(result, "na.action") <- NULL
+    if (!missing(along)) {
+        warning("along to be deprecated - use x instead")
+        if (missing(x)) x <- along
     }
 
-	result
+    missing.xout <- missing(xout) || is.null(xout)
+    if (is.function(x)) x <- x(index(object))
+    if (!missing.xout && is.function(xout)) xout <- xout(index(object))
+    order.by <- if (missing.xout) index(object) else xout
+    xout <- if (missing.xout) x else xout
+
+    if (missing.xout || identical(xout, index(object))) {
+        result <- object
+    } else {
+        object.x <- object
+        if (!identical(class(x), class(xout))) {
+            index(object.x) <- as.numeric(x)
+            xout <- as.numeric(xout)
+        } else {
+            index(object.x) <- x
+        }
+        objectm <- merge(object.x, zoo(, xout))
+        if (length(dim(objectm)) == 2) colnames(objectm) <- colnames(object)
+        result <- window(objectm, index = xout)
+    }
+    result[] <- na.approx.default(object, x = x, xout = xout, na.rm = FALSE, ...)
+    if ((!missing(order.by) && !is.null(order.by)) || !missing.xout) {
+        index(result) <- order.by
+    }
+
+    if (na.rm) {
+        result <- na.omit(result)
+        attr(result, "na.action") <- NULL
+    }
+
+    result
 
 }
 
 na.approx.zooreg <- function(object, ...) {
-	object. <- structure(object, class = setdiff(class(object), "zooreg"))
-	as.zooreg(na.approx(object., ...))
+    object. <- structure(object, class = setdiff(class(object), "zooreg"))
+    as.zooreg(na.approx(object., ...))
 }
 
 
 na.approx.default <- function(object, x = index(object), xout = x, ..., na.rm = TRUE, maxgap = Inf, along) {
 
-	if (!missing(along)) {
-		warning("along to be deprecated - use x instead")
-		if (missing(x)) x <- along
-	}
+    if (!missing(along)) {
+        warning("along to be deprecated - use x instead")
+        if (missing(x)) x <- along
+    }
 
-	na.approx.vec <- function(x, y, xout = x, ...) {
-		na <- is.na(y)
-		yf <- approx(x[!na], y[!na], xout, ...)$y
-		if (maxgap < length(y)) {
-		    ## construct version of y with only gaps > maxgap
-		    ygap <- .fill_short_gaps(y, seq_along(y), maxgap = maxgap)
-		    ## construct y values at 'x', keeping NAs from ygap
-		    ## (approx() does not allow NAs to be propagated)
-		    ix <- approx(x, seq_along(y), xout, ...)$y
-		    yx <- ifelse(is.na(ygap[floor(ix)] + ygap[ceiling(ix)]),
-					NA, yf)
-		    yx
-		} else {
-		    yf
-		}
-	}
+    na.approx.vec <- function(x, y, xout = x, ...) {
+        na <- is.na(y)
+        yf <- approx(x[!na], y[!na], xout, ...)$y
+        if (maxgap < length(y)) {
+            ## construct version of y with only gaps > maxgap
+            ygap <- .fill_short_gaps(y, seq_along(y), maxgap = maxgap)
+            ## construct y values at 'x', keeping NAs from ygap
+            ## (approx() does not allow NAs to be propagated)
+            ix <- approx(x, seq_along(y), xout, ...)$y
+            yx <- ifelse(is.na(ygap[floor(ix)] + ygap[ceiling(ix)]),
+                    NA, yf)
+            yx
+        } else {
+            yf
+        }
+    }
 
-	if (!identical(length(x), length(index(object)))) {
-		stop("x and index must have the same length")
-	}
-	x. <- as.numeric(x)
-	if (missing(xout) || is.null(xout)) xout <- x.
-	xout. <- as.numeric(xout)
-	object. <- coredata(object)
+    if (!identical(length(x), length(index(object)))) {
+        stop("x and index must have the same length")
+    }
+    x. <- as.numeric(x)
+    if (missing(xout) || is.null(xout)) xout <- x.
+    xout. <- as.numeric(xout)
+    object. <- coredata(object)
 
-	result <- if (length(dim(object.)) < 2) {
-		na.approx.vec(x., coredata(object.), xout = xout., ...)
-	} else {
-		apply(coredata(object.), 2, na.approx.vec, x = x., xout = xout., ...)
-	}
+    result <- if (length(dim(object.)) < 2) {
+        na.approx.vec(x., coredata(object.), xout = xout., ...)
+    } else {
+        apply(coredata(object.), 2, na.approx.vec, x = x., xout = xout., ...)
+    }
 
     if (na.rm) {
-		result <- na.omit(result)
+        result <- na.omit(result)
         attr(result, "na.action") <- NULL
-	}
+    }
 
-	result
+    result
 
 }
 
 na.approx.ts <- function(object, ...) {
-	as.ts(na.approx(as.zoo(object), ...))
+    as.ts(na.approx(as.zoo(object), ...))
 }
 
 ## x = series with gaps
@@ -112,6 +110,3 @@ na.approx.ts <- function(object, ...) {
     naok <- inverse.rle(naruns)
     ifelse(naok, fill, x)
 }
-
-
-
