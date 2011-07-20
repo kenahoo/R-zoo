af405d4737a5a26450a541ce54ed9976e9bf84e1
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-20 12:41:24 +0000

    some more touch-ups
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@874 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/as.Date.R
+++ R/as.Date.R
@@ -1,11 +1,10 @@
 as.Date <- function (x, ...) {
-	if (is.numeric(x)) zoo:::as.Date.numeric(x, ...)
-	else UseMethod("as.Date")
+  if (is.numeric(x)) zoo:::as.Date.numeric(x, ...)
+  else UseMethod("as.Date")
 }
 
 as.Date.numeric <- function (x, origin, ...) {
-    if (missing(origin)) origin <- "1970-01-01"
-    # Matalb origin is day before 0000-01-01
-	if (identical(origin, "0000-00-00")) as.Date("0000-01-01", ...) + x - 1
-    else as.Date(origin, ...) + x
-  }
+  if (missing(origin)) origin <- "1970-01-01"
+  if (identical(origin, "0000-00-00")) origin <- as.Date("0000-01-01", ...) - 1
+  as.Date(origin, ...) + x
+}
