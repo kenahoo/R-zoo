4bbca2c2cd25eb46c5c8b02512824e94813368ea
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-23 12:21:21 +0000

    as.Date.* was not being picked up from other packages.  exported everything and reverted as.Date stuff to earlier zoo version as that seemed to work properly.
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@897 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/as.Date.R
+++ R/as.Date.R
@@ -1,11 +1,11 @@
 as.Date <- function (x, ...) {
-  ## for plain numeric input, call zoo:::as.Date.numeric
-  if (!is.object(x) && is.numeric(x)) as.Date.numeric(x, ...)
-  else UseMethod("as.Date")
+	if (is.numeric(x)) zoo:::as.Date.numeric(x, ...)
+	else UseMethod("as.Date")
 }
 
 as.Date.numeric <- function (x, origin, ...) {
-  if (missing(origin)) origin <- "1970-01-01"
-  if (identical(origin, "0000-00-00")) origin <- as.Date("0000-01-01", ...) - 1
-  as.Date(origin, ...) + x
-}
+    if (missing(origin)) origin <- "1970-01-01"
+    # Matalb origin is day before 0000-01-01
+	if (identical(origin, "0000-00-00")) as.Date("0000-01-01", ...) + x - 1
+    else as.Date(origin, ...) + x
+  }
