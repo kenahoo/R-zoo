fe869318ac61c707785938bfc706379d4122d94f
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-24 11:38:06 +0000

    fixed as.Date.year* for all-NA vectors
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo/pkg/zoo@961 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/yearqtr.R
+++ R/yearqtr.R
@@ -39,14 +39,15 @@ as.yearqtr.ti <- function(x, ...) as.yearqtr(as.Date(x), ...)
 ## coercion from yearqtr
 # returned Date is the fraction of the way through the period given by frac
 as.Date.yearqtr <- function(x, frac = 0, ...) {
-	x <- unclass(x)
-	year <- floor(x + .001)
-    ix <- !is.na(year)
-	month <- floor(12 * (x - year) + 1 + .5 + .001)
-	dd.start <- as.Date(rep(NA, length(year)))
-    dd.start[ix] <- as.Date(paste(year[ix], month[ix], 1, sep = "-")) 
-	dd.end <- dd.start + 100 - as.numeric(format(dd.start + 100, "%d")) 
-	as.Date((1-frac) * as.numeric(dd.start) + frac * as.numeric(dd.end), origin = "1970-01-01")
+  x <- unclass(x)
+  if(all(is.na(x))) return(as.Date(x))
+  year <- floor(x + .001)
+  ix <- !is.na(year)
+  month <- floor(12 * (x - year) + 1 + .5 + .001)
+  dd.start <- as.Date(rep(NA, length(year)))
+  dd.start[ix] <- as.Date(paste(year[ix], month[ix], 1, sep = "-")) 
+  dd.end <- dd.start + 100 - as.numeric(format(dd.start + 100, "%d")) 
+  as.Date((1-frac) * as.numeric(dd.start) + frac * as.numeric(dd.end), origin = "1970-01-01")
 }
 as.POSIXct.yearqtr <- function(x, tz = "", ...) as.POSIXct(as.Date(x), tz = tz, ...)
 as.POSIXlt.yearqtr <- function(x, tz = "", ...) as.POSIXlt(as.Date(x), tz = tz, ...)
